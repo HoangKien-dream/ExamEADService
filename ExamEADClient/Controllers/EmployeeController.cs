467d1fc56ed049d918253b84e706ae47681970d1
Kien <kien@gmail.com>
2022-05-10 22:06:37 +0700

    NoHope

---
+++ ExamEADClient/Controllers/EmployeeController.cs
@@ -0,0 +1,102 @@
+﻿using ExamEADClient.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExamEADClient.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        Service1Client service;
+
+        public EmployeeController()
+        {
+            service = new Service1Client();
+        }
+        // GET: Employee
+        public ActionResult Index(string searchString)
+        {
+            if(searchString == null)
+            {
+                return View(service.FindAll());
+            }
+            return View(service.FindByDeparment(searchString)); 
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            try
+            {
+                service.Save(employee);
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ExamEADClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExamEADClient.MvcApplication" Language="C#" %>
